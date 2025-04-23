9401d0fcbc33baf28d0987c424de30523569e58c
Rahul Bisht <bishtrahul6400@gmail.com>
2025-04-23 13:52:33 +0530

    Done with the Products page and also category wise products page

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -15,6 +15,7 @@ export const AppContextProvider = ({ children }) => {
   const [showUserLogin, setShowUserLogin] = useState(false);
   const [products, setProducts] = useState([]);
   const [cartItems, setCartItems] = useState({});
+  const [searchQuery, setSearchQuery] = useState({});
 
   //fetch all products
   const fetchProducts = async () => {
@@ -59,7 +60,7 @@ export const AppContextProvider = ({ children }) => {
   },[])
 
   const value = { navigate, user, setUser, isSeller, setIsSeller, showUserLogin, setShowUserLogin
-    , products, currency, addToCart, updateCartItems, removeFromCart, cartItems
+    , products, currency, addToCart, updateCartItems, removeFromCart, cartItems, searchQuery, setSearchQuery
   };
 
   return (
