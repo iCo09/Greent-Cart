ccc2fc2e7216f663402b3ebf15ced81aaf945300
Rahul Bisht <bishtrahul6400@gmail.com>
2025-04-25 15:14:52 +0530

    Completed the ProductsDetails Page

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -54,13 +54,35 @@ export const AppContextProvider = ({ children }) => {
     setCartItems(cartData);
   }
 
+  //Get cart item count
+  const getCartCount = () => {
+    let totalCount = 0;
+    for(const item in cartItems){
+      totalCount += cartItems[item];
+    }
+    return totalCount;
+  }
+
+  //Get cart total amount
+  const getCartAmount = () => {
+    let totalAmount = 0;
+    for(const items in cartItems){
+      let itemInfo = products.find((product) => product._id === items);
+      if(cartItems[items] > 0){
+        totalAmount += itemInfo.offerPrice * cartItems[items];
+      }
+    }
+    return Math.floor(totalAmount * 100) / 100;
+  }
+
 
   useEffect(() => {
     fetchProducts();
   },[])
 
   const value = { navigate, user, setUser, isSeller, setIsSeller, showUserLogin, setShowUserLogin
-    , products, currency, addToCart, updateCartItems, removeFromCart, cartItems, searchQuery, setSearchQuery
+    , products, currency, addToCart, updateCartItems, removeFromCart, cartItems, searchQuery, setSearchQuery,
+    getCartAmount, getCartCount
   };
 
   return (
