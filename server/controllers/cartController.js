ab5d1a6e90d189d694812705a3f657c8ff52f0ae
Rahul Bisht <bishtrahul6400@gmail.com>
2025-05-10 00:08:12 +0530

    Fixed the issue of saving the cartItems in DB for each user

--- server/controllers/cartController.js
+++ server/controllers/cartController.js
@@ -3,11 +3,18 @@ import User from "../models/User.js"
 //Update user cart :/api/cart/update
 export const updateCart = async (req, res) => {
     try {
-        const {userId, cartItems} = req.body
-        await User.findByIdAndUpdate(userId, {cartItems})
-        res.json({success: true, message: "Cart updated successfully"})
+      const { cartItems } = req.body;
+      const userId = req.user.id;
+  
+      if (!userId) {
+        return res.json({ success: false, message: "User ID missing" });
+      }
+  
+      await User.findByIdAndUpdate(userId, { cartItems });
+      res.json({ success: true, message: "Cart updated successfully" });
     } catch (error) {
-        console.log(error.message)
-        res.json({success: false, message: error.message})
+      console.log("Update cart error:", error.message);
+      res.json({ success: false, message: error.message });
     }
-}
+  };
+  
--- server/routes/cartRoute.js
+++ server/routes/cartRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import authUser from "../middlewares/authSeller.js";
+import authUser from "../middlewares/authUser.js"
 import { updateCart } from "../controllers/cartController.js";
 
 
